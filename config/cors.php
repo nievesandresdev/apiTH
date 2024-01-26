c887b8437063526897e1a0416dec545d10a008b3
Xavier Pestana <xpestana4@gmail.com>
2024-01-26 14:56:34 -0400

    asdad

--- config/cors.php
+++ config/cors.php
@@ -15,17 +15,13 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['*'],
 
     'allowed_methods' => ['*'],
 
     'allowed_origins' => ['*'],
 
-    'allowed_origins_patterns' => [
-        '/^https?:\/\/(.+\.)?thehoster\.io$/',
-        '/^http:\/\/localhost(:[0-9]+)?$/',
-        '/^http:\/\/127\.0\.0\.1(:[0-9]+)?$/'
-    ],
+    'allowed_origins_patterns' => [],
 
     'allowed_headers' => ['*'],
 
