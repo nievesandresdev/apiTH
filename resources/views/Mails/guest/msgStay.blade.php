298e22d76ee56163fdee21d17f9d44ded8d29be3
Fran <francisco20990@gmail.com>
2024-05-13 10:56:18 -0400

    test mail

--- resources/views/Mails/guest/msgStay.blade.php
+++ resources/views/Mails/guest/msgStay.blade.php
@@ -1,3 +1,26 @@
-<p>
+{{-- <p style="color: red">
     {!! $msg !!}
-</p> 
+</p> --}}
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+        body {
+            background-image: url('{{ asset("mails/fondo.jpg") }}');
+            background-size: cover;
+            background-position: center;
+            height: auto;
+        }
+        /* Estilos adicionales para el contenido del correo */
+    </style>
+</head>
+<body style="background-image: url('{{ asset("mails/fondo.jpg") }}')">
+    <p>Hola {{ asset("mails/fondo.jpg") }},</p>
+    <img src="{{ asset('mails/logo.png') }}" alt="Logo de la empresa">
+    {!! $msg !!}
+</body>
+</html>
+
