c2b31421f2f961acce3e0fe4a80e54c52f599816
Fran <francisco20990@gmail.com>
2024-05-14 06:36:20 -0400

    test mail

--- resources/views/Mails/guest/msgStay.blade.php
+++ resources/views/Mails/guest/msgStay.blade.php
@@ -10,7 +10,8 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 </head>
-<body style="background-image: url('{{ asset("mails/mail.png") }}'); background-size: cover;  height: 6100px;font-family: Montserrat;">
+<body style="background-image: url('{{ asset("mails/mail.png") }}'); background-size: cover; height: 6100px; font-family: 'Montserrat', sans-serif;">
+
 
 
 </body>
