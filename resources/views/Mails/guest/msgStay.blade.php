027f7b348a873b8b2fde819b9e455f4a9e0c342d
Fran <francisco20990@gmail.com>
2024-05-14 07:30:31 -0400

    mail template test

--- resources/views/Mails/guest/msgStay.blade.php
+++ resources/views/Mails/guest/msgStay.blade.php
@@ -10,18 +10,18 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
     {{-- app.css --}}
-    <style media="all" type="text/css">
+    <style>
 
         /* Media queries para ajustar la imagen de fondo en dispositivos más pequeños */
         @media only screen and (max-width: 600px) {
           body {
-            background-size: contain; /* O puedes ajustar con valores específicos como '100% auto' */
-            height: 20%; /* Ajusta la altura según el contenido o un valor específico que necesites */
+            background-size: contain;
+            height: 10%;
           }
         }
       </style>
 </head>
-<body style="background-image: url('{{ asset("mails/mail.png") }}'); background-size: cover; height: 100%; font-family: 'Montserrat', sans-serif;">
+<body style="background-image: url('{{ asset("mails/mail.png") }}'); background-size: cover; height: 6100px; font-family: 'Montserrat', sans-serif;">
 
 
 
