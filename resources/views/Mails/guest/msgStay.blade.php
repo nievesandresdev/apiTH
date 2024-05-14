8c5beb7433c5bd227393ab4e73b0d2e912b50d1f
Fran <francisco20990@gmail.com>
2024-05-14 06:25:31 -0400

    bg mail test

--- resources/views/Mails/guest/msgStay.blade.php
+++ resources/views/Mails/guest/msgStay.blade.php
@@ -10,11 +10,8 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 </head>
-<body style="background-image: url('{{ asset("mails/mail.png") }}'); background-size: cover; height: auto;font-family: Montserrat;">
-    <div style="position: absolute; right: 20px; bottom: 20px; text-align: right; font-size: 36px; color: #333; padding: 4em">
-        <p style="margin-top: 50px;">Te damos la bienvenida a <br>[Hotel]</p>
-        {{-- <p style="margin: 0; font-size: 0.5em; color: #666;">escribe algo</p> --}}
-    </div>
+<body style="background-image: url('{{ asset("mails/mail.png") }}'); background-size: cover;  height: 6100px;font-family: Montserrat;">
+
 
 </body>
 </html>
