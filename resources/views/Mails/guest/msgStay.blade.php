6e1440b295ef3a41f2f6848c099fe0abb51587eb
Fran <francisco20990@gmail.com>
2024-05-14 06:14:51 -0400

    test image mail

--- resources/views/Mails/guest/msgStay.blade.php
+++ resources/views/Mails/guest/msgStay.blade.php
@@ -10,25 +10,11 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 </head>
-<body style="background-image: url('{{ asset("mails/fondo.jpg") }}'); background-size: cover; background-position: center; height: 2048px;font-family: Montserrat;">
-    {{-- <div style="display: flex; justify-content: center; align-items: center; height: 100vh; background: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url('{{ asset("mails/alarma.png") }}'); background-size: cover; background-position: center;">
-    </div> --}}
-    {{-- <div style="display: flex; justify-content: center; align-items: center; text-align: center; height: 100vh; background: linear-gradient(to bottom, rgba(255, 255, 255, 0.8), rgba(255, 255, 255, 0.8)), url('{{ asset("mails/alarma.png") }}'); background-size: cover; background-position: center;">
-        <div style="font-size: 2em; color: #333;">
-            <p style="margin: 10rem;">ESCRIBE ALGO</p>
-            <p style="margin: 0;">Escribe algo</p>
-        </div>
-    </div> --}}
-    <div style="position: relative; height: 100vh; background: linear-gradient(to bottom, rgba(255, 255, 255, 0.8), rgba(255, 255, 255, 0.8)), url('{{ asset("mails/alarma.png") }}'); background-size: cover; background-position: center;">
-        <div style="position: absolute; right: 20px; bottom: 20px; text-align: right; font-size: 36px; color: #333; padding : 3em">
-            <p style="margin-top: 30px;">Te damos la bienvenida a <br>[Hotel]</p>
-            {{-- <p style="margin: 0; font-size: 0.5em; color: #666;">escribe algo</p> --}}
-        </div>
+<body style="background-image: url('{{ asset("mails/mail.png") }}'); background-size: cover; height: auto;font-family: Montserrat;">
+    <div style="position: absolute; right: 20px; bottom: 20px; text-align: right; font-size: 36px; color: #333; padding: 4em">
+        <p style="margin-top: 50px;">Te damos la bienvenida a <br>[Hotel]</p>
+        {{-- <p style="margin: 0; font-size: 0.5em; color: #666;">escribe algo</p> --}}
     </div>
-
-
-
-    {{-- <span>ESCRIBE ALGO</span> --}}
 
 </body>
 </html>
