d1e6e4fb8d21cdb128cdf2871876444fff4afb5b
Fran <francisco20990@gmail.com>
2024-05-14 21:44:51 -0400

    test mail

--- resources/views/Mails/guest/msgStay.blade.php
+++ resources/views/Mails/guest/msgStay.blade.php
@@ -28,6 +28,9 @@
     </div>
 
     <img src="{{ asset("mails/1.png") }}" alt="1">
+    <img src="{{ asset("mails/2.png") }}" alt="2">
+    <img src="{{ asset("mails/3.png") }}" alt="3">
+    <img src="{{ asset("mails/4.png") }}" alt="4">
 
 </body>
 </html>
