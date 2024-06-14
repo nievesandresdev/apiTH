b2942f9ab7ffa1838a091937820d451b65b8978e
xpestana <unibasileaweb@gmail.com>
2024-06-14 19:42:52 -0400

    passport

--- config/passport.php
+++ config/passport.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'guard' => 'web',
+    'guard' => 'api',
 
     /*
     |--------------------------------------------------------------------------
@@ -26,9 +26,9 @@ return [
     |
     */
 
-    'private_key' => env('PASSPORT_PRIVATE_KEY'),
+    'private_key' => storage_path('oauth-private.key'),
 
-    'public_key' => env('PASSPORT_PUBLIC_KEY'),
+    'public_key' => storage_path('oauth-public.key'),
 
     /*
     |--------------------------------------------------------------------------
