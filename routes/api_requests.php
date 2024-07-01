6d39bb3fce1b1b5a00f98e210c619835807df8b9
Andres <andresdreamerf@gmail.com>
2024-07-01 12:25:50 -0400

    route request

--- routes/api_requests.php
+++ routes/api_requests.php
@@ -7,4 +7,7 @@ use Illuminate\Support\Facades\Route;
 Route::group(['prefix' => 'request-settings'], function () {
     Route::get('/getAll', [RequestSettingController::class, 'getAll']);
     Route::get('/getPostStayRequestData', [RequestSettingController::class, 'getPostStayRequestData']);
+    Route::group(['prefix' => 'hoster'], function () {
+        Route::get('/updateData', [RequestSettingController::class, 'updateData']);
+    });
 });
