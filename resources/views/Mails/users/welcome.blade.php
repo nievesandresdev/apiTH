444d45c1e2a28447ad4e5aef2ecdc51078f24e25
Fran <francisco20990@gmail.com>
2024-07-09 11:11:40 -0400

    test mail flex

--- resources/views/Mails/users/welcome.blade.php
+++ resources/views/Mails/users/welcome.blade.php
@@ -11,13 +11,13 @@
         </div>
         <div style="border-radius: 0px 72px 0px 0px; background: linear-gradient(90deg, #0B6357 -17.99%, #34A98F 118.23%); padding: 20px;">
             <div style="display: flex; align-items: center;">
-                <div style="color: white; padding-right: 30px; flex: 0; text-align: left;">
+                <div style="color: white; padding-right: 30px; flex: 1; text-align: left;">
                     <h1 style="margin: 0;">¡Bienvenido a Thehoster!</h1>
                     <p style="margin: 10px 0;">[Nombre del usuario] ha creado un usuario en la plataforma para que administres el [tipo de alojamiento] [nombre alojamiento]</p>
                     <p style="margin: 10px 0;">¡Gracias por elegirnos!</p>
                 </div>
                 <div style="text-align: center; flex: 1;">
-                    <img src="{{ asset('mails/users/banner.png') }}" alt="Welcome Banner" style="width: 350px; height: 240px;">
+                    <img src="{{ asset('mails/users/banner.png') }}" alt="Welcome Banner" style="width: 255px; height: 240px;">
                 </div>
             </div>
         </div>
