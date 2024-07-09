8a3e6f1c14919150bc928955979897e93a96be92
Fran <francisco20990@gmail.com>
2024-07-09 11:51:14 -0400

    test mail flex

--- resources/views/Mails/users/welcome.blade.php
+++ resources/views/Mails/users/welcome.blade.php
@@ -5,18 +5,15 @@
     <title>Bienvenido a Thehoster</title>
     <style>
         @media only screen and (max-width: 600px) {
-            .flex-container {
-                display: block !important;
-                text-align: center !important;
-            }
-            .flex-item {
+            .responsive-table {
                 width: 100% !important;
-                margin-bottom: 20px !important;
-                padding: 0 !important;
-                text-align: center !important;
+                display: block !important;
             }
-            .flex-item img {
-                margin: 0 auto !important;
+            .responsive-table td {
+                display: block !important;
+                width: 100% !important;
+                text-align: center !important;
+                padding: 10px 0 !important;
             }
         }
     </style>
@@ -27,16 +24,18 @@
             <img src="{{ asset('mails/users/logo.png') }}" alt="Thehoster Logo" style="display: block; margin: 0 auto;">
         </div>
         <div style="border-radius: 0px 72px 0px 0px; background: linear-gradient(90deg, #0B6357 -17.99%, #34A98F 118.23%); padding: 20px;">
-            <div class="flex-container" style="display: flex; align-items: center; justify-content: space-between;">
-                <div class="flex-item" style="color: white; padding-right: 40px; flex: 1; text-align: left;">
-                    <h1 style="margin: 0;">¡Bienvenido a Thehoster!</h1>
-                    <p style="margin: 10px 0;">[Nombre del usuario] ha creado un usuario en la plataforma para que administres el [tipo de alojamiento] [nombre alojamiento]</p>
-                    <p style="margin: 10px 0;">¡Gracias por elegirnos!</p>
-                </div>
-                <div class="flex-item" style="text-align: center; flex: 1;">
-                    <img src="{{ asset('mails/users/banner.png') }}" alt="Welcome Banner" style="width: 255px; height: 240px;">
-                </div>
-            </div>
+            <table class="responsive-table" role="presentation" width="100%" cellspacing="0" cellpadding="0" border="0">
+                <tr>
+                    <td style="color: white; padding-right: 40px; text-align: left; width: 50%; vertical-align: top;">
+                        <h1 style="margin: 0;">¡Bienvenido a Thehoster!</h1>
+                        <p style="margin: 10px 0;">[Nombre del usuario] ha creado un usuario en la plataforma para que administres el [tipo de alojamiento] [nombre alojamiento]</p>
+                        <p style="margin: 10px 0;">¡Gracias por elegirnos!</p>
+                    </td>
+                    <td style="text-align: center; width: 50%; vertical-align: top;">
+                        <img src="{{ asset('mails/users/banner.png') }}" alt="Welcome Banner" style="width: 255px; height: 240px;">
+                    </td>
+                </tr>
+            </table>
         </div>
     </div>
 </body>
