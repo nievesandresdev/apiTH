fcdcbb633a1bf5f3ddadb78727c055d876bb8150
Fran <francisco20990@gmail.com>
2024-07-09 10:57:59 -0400

    test mail flex

--- resources/views/Mails/users/welcome.blade.php
+++ resources/views/Mails/users/welcome.blade.php
@@ -3,21 +3,6 @@
 <head>
     <meta charset="UTF-8">
     <title>Bienvenido a Thehoster</title>
-    <style>
-        @media screen and (max-width: 600px) {
-            .flex-container {
-                flex-direction: column !important;
-                text-align: center !important;
-            }
-            .banner-img {
-                width: 216px !important;
-                height: 240px !important;
-            }
-            .text-content {
-                text-align: center !important;
-            }
-        }
-    </style>
 </head>
 <body style="margin: 0; padding: 0; font-family: Arial, sans-serif; background: #ffffff;">
     <div style="width: 100%; max-width: 600px; margin: 0 auto;">
@@ -25,14 +10,14 @@
             <img src="{{ asset('mails/users/logo.png') }}" alt="Thehoster Logo" style="display: block; margin: 0 auto;">
         </div>
         <div style="border-radius: 0px 72px 0px 0px; background: linear-gradient(90deg, #0B6357 -17.99%, #34A98F 118.23%); padding: 20px;">
-            <div class="flex-container" style="display: flex; align-items: center;">
-                <div class="text-content" style="color: white; padding-right: 20px; flex: 1; text-align: left;">
+            <div style="display: flex; align-items: center;">
+                <div style="color: white; padding-right: 20px; flex: 1; text-align: left;">
                     <h1 style="margin: 0;">¡Bienvenido a Thehoster!</h1>
                     <p style="margin: 10px 0;">[Nombre del usuario] ha creado un usuario en la plataforma para que administres el [tipo de alojamiento] [nombre alojamiento]</p>
                     <p style="margin: 10px 0;">¡Gracias por elegirnos!</p>
                 </div>
                 <div style="text-align: center; flex: 1;">
-                    <img class="banner-img" src="{{ asset('mails/users/banner.png') }}" alt="Welcome Banner" style="width: 216px; height: 240px;">
+                    <img src="{{ asset('mails/users/banner.png') }}" alt="Welcome Banner" style="width: 216px; height: 240px;">
                 </div>
             </div>
         </div>
