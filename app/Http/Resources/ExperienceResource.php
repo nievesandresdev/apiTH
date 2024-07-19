a4a9351019c03d50ec81ac3362fa2435d1ce070f
jhjacoz96 <jhjacoz96@gmail.com>
2024-07-19 12:45:09 -0400

    fix

--- app/Http/Resources/ExperienceResource.php
+++ app/Http/Resources/ExperienceResource.php
@@ -20,6 +20,10 @@ class ExperienceResource extends JsonResource
         if($this->distance){
             $distance = round($this->distance / 1000, 2);
         }
+
+        $isVisible = $this->toggleableHotels()->where('hotel_id', $hotel->id)->exists() &&
+        !$this->productHidden()->where('hotel_id', $hotel->id)->exists();
+
         return [
             'id' => $this->id,
             'image' => $this->images()->orderBy('id','ASC')->first(),
@@ -31,6 +35,7 @@ class ExperienceResource extends JsonResource
             'select' => $this->select,
             'from_price' => $this->from_price,
             'reviews' => $this->reviews,
+            'is_visible' => boolval($isVisible),
             'recomendations' => $this->recomendations()->where('hotel_id', $modelHotel->id)->first(),
             'product_featured' => $this->productFeatured()->where('hotel_id', $modelHotel->id)->first(),
             // activities
