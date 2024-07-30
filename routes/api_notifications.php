febc10a8a961df8b6d3a7ba4d71f63df667412e6
Andres <andresdreamerf@gmail.com>
2024-07-30 16:26:07 -0400

    route

--- routes/api_notifications.php
+++ routes/api_notifications.php
@@ -4,6 +4,6 @@ use App\Http\Controllers\Api\Hoster\NotificationsController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::group(['prefix' => 'notifications'], function () {
+Route::group(['prefix' => 'hoster/notifications'], function () {
     Route::get('/getNotificationsByUser', [NotificationsController::class, 'getNotificationsByUser']);
 });
