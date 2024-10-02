1704ac301b715701cc8e5312b0f44c6ff863cd41
Fran <francisco20990@gmail.com>
2024-10-02 19:09:44 -0400

    add icons linkdin and web email footer

--- resources/views/components/mails/footer.blade.php
+++ resources/views/components/mails/footer.blade.php
@@ -3,6 +3,17 @@
 @endphp
 <div style="background-color: #1A1A1A; padding: 32px; text-align: center; color: #ffffff;">
     <img src="{{ asset('mails/logo-white.png') }}" alt="Thehoster Logo" style="display: block; margin: 0 auto 20px;">
+
+    <!-- New icons for web and LinkedIn -->
+    <div style="margin-bottom: 20px;">
+        <a href="https://thehoster.io/" target="_blank" style="margin-right: 16px;">
+            <img src="{{ asset('mails/web.png') }}" alt="Website Icon" style="width: 24px; height: 24px;">
+        </a>
+        <a href="https://www.linkedin.com/company/the-hoster/" target="_blank">
+            <img src="{{ asset('mails/linkedin.png') }}" alt="LinkedIn Icon" style="width: 24px; height: 24px;">
+        </a>
+    </div>
+
     <div class="responsive-footer-links" style="margin-bottom: 24px; margin-top:16px;">
         <a href="https://thehoster.io/aviso-legal" style="color: #A0A0A0; text-decoration: none; font-size: 12px; font-style: normal; font-weight: 400; line-height: 150%; margin-right: 15px;">Aviso legal</a>
         <a href="https://thehoster.io/privacidad" style="color: #A0A0A0; text-decoration: none; font-size: 12px; font-style: normal; font-weight: 400; line-height: 150%; margin-right: 15px;">Política de privacidad</a>
