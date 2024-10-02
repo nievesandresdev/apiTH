fbe0c16ff36f43356d30bc350285e06992afab0a
Fran <francisco20990@gmail.com>
2024-10-02 13:22:15 -0400

    fix footer url

--- resources/views/components/mails/footer.blade.php
+++ resources/views/components/mails/footer.blade.php
@@ -6,7 +6,7 @@
     <div class="responsive-footer-links" style="margin-bottom: 24px; margin-top:16px;">
         <a href="https://thehoster.io/aviso-legal" style="color: #A0A0A0; text-decoration: none; font-size: 12px; font-style: normal; font-weight: 400; line-height: 150%; margin-right: 15px;">Aviso legal</a>
         <a href="https://thehoster.io/privacidad" style="color: #A0A0A0; text-decoration: none; font-size: 12px; font-style: normal; font-weight: 400; line-height: 150%; margin-right: 15px;">Política de privacidad</a>
-        <a href="{{$url}}/equipo/configuracion/usuarios?n={{auth()->user()->id}}" class="config-notifications" style="color: #A0A0A0; text-decoration: none; font-size: 12px; font-style: normal; font-weight: 400; line-height: 150%;">Configurar notificaciones</a>
+        <a href="{{$url}}equipo/configuracion/usuarios?n={{auth()->user()->id}}" class="config-notifications" style="color: #A0A0A0; text-decoration: none; font-size: 12px; font-style: normal; font-weight: 400; line-height: 150%;">Configurar notificaciones</a>
     </div>
     <p style="margin: 12px 0 0; font-size: 12px; font-style: normal; font-weight: 400; line-height: 150%;">© Copyright TheHoster 2024</p>
 </div>
