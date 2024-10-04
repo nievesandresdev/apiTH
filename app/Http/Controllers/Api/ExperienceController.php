36cc52e121fdd0b5581a4972c9eeeefc793857b2
jhjacoz96 <jhjacoz96@gmail.com>
2024-10-04 12:26:12 -0400

    fix

--- app/Http/Controllers/Api/ExperienceController.php
+++ app/Http/Controllers/Api/ExperienceController.php
@@ -68,7 +68,6 @@ class ExperienceController extends Controller
             ];
 
             $response = $this->service->getAll($request, $modelHotel, $dataFilter);
-            return $response;
             $expsCollection = $response['experiences'];
             $countOtherCities = $response['countOtherCities'];
             $data = [
