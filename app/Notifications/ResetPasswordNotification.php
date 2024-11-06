2b1dadc247e6de974b052733e4fccf5456c86a87
Fran <francisco20990@gmail.com>
2024-11-06 08:52:04 -0400

    fix reset password

--- app/Notifications/ResetPasswordNotification.php
+++ app/Notifications/ResetPasswordNotification.php
@@ -25,7 +25,7 @@ class ResetPasswordNotification extends Notification
         return (new MailMessage)
                     ->subject(Lang::get('Restablecimiento de contraseña'))
                     ->line(Lang::get('Está recibiendo este correo electrónico porque recibimos una solicitud de restablecimiento de contraseña para su cuenta.'))
-                    ->action(Lang::get('Restablecer contraseña'), url(config('app.url')."reset-password/{$this->token}?email={$notifiable->getEmailForPasswordReset()}"))
+                    ->action(Lang::get('Restablecer contraseña'), config('app.hoster_url')."reset-password/{$this->token}?email={$notifiable->getEmailForPasswordReset()}")
                     ->line(Lang::get('Este enlace de restablecimiento de contraseña expirará en :count minutos.', ['count' => config('auth.passwords.users.expire')]))
                     ->line(Lang::get('Si no solicitó un restablecimiento de contraseña, no se requiere ninguna acción adicional.'));
     }
