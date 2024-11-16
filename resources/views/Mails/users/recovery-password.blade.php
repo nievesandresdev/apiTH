7d37833afd28d9c3139045e1d87b6248c7651bd0
Fran <francisco20990@gmail.com>
2024-11-16 09:25:00 -0400

    test pass

--- resources/views/Mails/users/recovery-password.blade.php
+++ resources/views/Mails/users/recovery-password.blade.php
@@ -5,36 +5,36 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ __('Restablecimiento de contraseña') }}</title>
-    <!-- Enlace a Google Fonts para Montserrat -->
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500&display=swap" rel="stylesheet">
+    <!-- Enlace a Google Fonts para Montserrat con todos los pesos -->
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
 </head>
 <body style="background-color: #ffffff; font-family: 'Montserrat', Arial, sans-serif; margin: 0; padding: 20px;">
     <div style="max-width: 600px; margin: auto; background-color: #ffffff; border: 1px solid #ddd; border-radius: 8px; padding: 20px;">
 
         <!-- Primer Párrafo -->
-        <p style="font-size: 18px; font-style: normal; font-weight: 500; line-height: 28px; color: #333; text-align: left;">
+        <p style="font-size: 18px; font-style: normal; font-weight: 400; line-height: 28px; color: #333; text-align: left;">
             Has solicitado un enlace para restablecer la contraseña de tu cuenta en TheHoster. Haz click en el siguiente botón para iniciar el proceso.
         </p>
 
         <!-- Botón Centrado -->
         <div style="text-align: center; margin: 30px 0;">
             <a href="{{ config('app.hoster_url') . 'reset-password/' . $token . '?email=' . $notifiable->getEmailForPasswordReset() }}"
-               style="background-color: #FFD453; color: #000; padding: 10px 20px; border-radius: 5px; font-size: 18px; font-weight: 500; text-decoration: none; display: inline-block;">
+               style="background-color: #FFD453; color: #000; padding: 10px 20px; border-radius: 5px; font-size: 18px; font-weight: 400; text-decoration: none; display: inline-block;">
                 {{ __('Restablecer contraseña') }}
             </a>
         </div>
 
         <!-- Segundo Párrafo -->
-        <p style="font-size: 18px; font-style: normal; font-weight: 500; line-height: 28px; color: #333; text-align: left;">
+        <p style="font-size: 18px; font-style: normal; font-weight: 400; line-height: 28px; color: #333; text-align: left;">
             Este enlace de restablecimiento de contraseña expira en 60 minutos.
         </p>
 
-        <p style="font-size: 18px; font-style: normal; font-weight: 500; line-height: 28px; color: #333; text-align: left;">
+        <p style="font-size: 18px; font-style: normal; font-weight: 400; line-height: 28px; color: #333; text-align: left;">
             Si no solicitaste un restablecimiento de la contraseña, no se requiere ninguna acción.
         </p>
 
         <!-- Saludo Final -->
-        <p style="font-size: 18px; font-style: normal; font-weight: 500; line-height: 28px; color: #333; text-align: left;">
+        <p style="font-size: 18px; font-style: normal; font-weight: 400; line-height: 28px; color: #333; text-align: left;">
             Saludos,<br>
             TheHoster
         </p>
@@ -44,7 +44,7 @@
         <div style="background: #DADADA; height: 1px; margin-bottom: 16px;"></div>
 
         <!-- Mensaje de URL alternativa -->
-        <p style="font-size: 18px; font-style: normal; font-weight: 500; line-height: 28px; color: #333; text-align: left;">
+        <p style="font-size: 18px; font-style: normal; font-weight: 400; line-height: 28px; color: #333; text-align: left;">
             Si tienes problemas tras pulsar en el botón “Restablecer contraseña”, copia y pega el enlace en tu navegador para restablecerla:
             <a href="{{ config('app.hoster_url') . 'reset-password/' . $token . '?email=' . $notifiable->getEmailForPasswordReset() }}"
                style="color: #5E7DD4; text-decoration: none; word-wrap: break-word; word-break: break-all;">
