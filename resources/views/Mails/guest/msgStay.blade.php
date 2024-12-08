2f8eb3083665ecc77b5fba500877b8a640052659
Fran <francisco20990@gmail.com>
2024-12-08 09:04:03 -0400

    fix

--- resources/views/Mails/guest/msgStay.blade.php
+++ resources/views/Mails/guest/msgStay.blade.php
@@ -182,55 +182,6 @@
         </section>
 
 
-
-
-
-
-
-
-
-
-
-
-        <!-- Nueva sección añadida aquí -->
-        {{-- <div style="background-color: white; padding-top: 32px; padding-bottom: 64px; text-align: center; padding-right: 32px; padding-left: 32px;" class="div-normal">
-            <table class="responsive-table-2" role="presentation" width="100%" cellspacing="0" cellpadding="0" border="0" style="table-layout: fixed;">
-                <tr style="height: 100%;">
-                    <td style="width: 40%; vertical-align: top; padding-right: 32px;">
-                        <img src="{{ asset('mails/users/frame.png') }}" alt="Frame Image" style="width: 100%; height: 100%; border-radius: 8px; object-fit: cover;" class="image-frame">
-                    </td>
-                    <td class="text-content" style="width: 60%; vertical-align: top; text-align: left;">
-                        <h2 style="margin: 0;">Datos de acceso</h2>
-                        <p style="margin: 10px 0; font-weight: 400;">Estas son tus credenciales de acceso a la plataforma.</p>
-                        <p style="margin: 10px 0;"><strong>Usuario:</strong> <span style="font-weight: 400;">{{ $user->email }}</span></p>
-                        <p style="margin: 10px 0;"><strong>Contraseña:</strong> <span style="font-weight: 400;">{{ $password }}</span></p>
-                        <a href="{{ $url }}" style="display: inline-block; padding-top:13px;padding-bottom:13px; background-color: #FFD453; color: #000; text-decoration: none; border-radius: 4px; margin-top: 27px; font-weight: 500; width: 100%; box-sizing: border-box; text-align: center;font-size: 16px;font-style: normal;line-height: 110%;">Entrar a Thehoster</a>
-                    </td>
-                </tr>
-            </table>
-        </div> --}}
-
-
-
-        {{-- <div style="background-color: white; padding: 20px; text-align: center;" class="div-responsive">
-            <table class="responsive-table-2" role="presentation" width="100%" cellspacing="0" cellpadding="0" border="0" style="table-layout: fixed;">
-                <tr>
-                    <td class="text-content order-1" style="width: 60%; vertical-align: top; text-align: left;">
-                        <h2 style="margin: 0;">Datos de acceso</h2>
-                        <p style="margin: 10px 0; font-weight: 400;">Estas son tus credenciales de acceso a la plataforma.</p>
-                        <p style="margin: 10px 0;"><strong>Usuario:</strong> <span style="font-weight: 400;">{{ $user->email }}</span></p>
-                        <p style="margin: 10px 0;"><strong>Contraseña:</strong> <span style="font-weight: 400;">{{ $password }}</span></p>
-                    </td>
-                    <td class="text-content order-2" style="width: 60%; vertical-align: top;">
-                        <a href="{{ $url }}" style="display: inline-block; padding: 10px 20px; background-color: #FFD453; color: #000; text-decoration: none; border-radius: 4px; margin-top: 10px; font-weight: 500; width: 100%; box-sizing: border-box; text-align: center;">Entrar a Thehoster</a>
-                    </td>
-                    <td class="order-3" style="width: 40%; vertical-align: top; padding-right: 20px;">
-                        <img src="{{ asset('mails/users/frame.png') }}" alt="Frame Image" style="width: 100%; height: 187px; border-radius: 8px; max-height: 300px;" class="image-frame">
-                    </td>
-                </tr>
-            </table>
-        </div> --}}
-
         <!-- Footer -->
         {{-- @include('components.mails.footer',['showNotify' => false]) --}}
     </div>
