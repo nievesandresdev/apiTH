fee4d20ccc19fafa29c9ce2bc8ddf0181b2781e9
Fran <francisco20990@gmail.com>
2024-12-08 09:23:39 -0400

    test email

--- resources/views/Mails/guest/msgStay.blade.php
+++ resources/views/Mails/guest/msgStay.blade.php
@@ -133,7 +133,7 @@
             </p>
 
             <!-- Tabla de íconos -->
-            <table role="presentation" width="100%" cellspacing="0" cellpadding="0" border="0" style="margin-top: 16px; text-align: center; padding-left : 50px; padding-right: 60px ; width:100%">
+            <table role="presentation" width="100%" cellspacing="0" cellpadding="0" border="0" style="margin-top: 16px; text-align: center; padding-left : 40px; padding-right: 50px">
                 <tr>
                     <!-- Muy Mala -->
                     <td style="text-align: center; padding: 0 30px;">
