0e179eae7aa8cf884b09ca142ce9940eee9edadb
Fran <francisco20990@gmail.com>
2024-12-13 13:05:31 -0400

    test email data

--- resources/views/components/mails/qrHotel.blade.php
+++ resources/views/components/mails/qrHotel.blade.php
@@ -1,15 +1,15 @@
-<table width="100%" cellpadding="0" cellspacing="0" border="0" style="padding: 20px; font-family: Arial, sans-serif;">
+<table width="100%" cellpadding="0" cellspacing="0" border="0" style="padding: 10px; font-family: Arial, sans-serif;">
     <tr>
       <td align="center">
-        <table cellpadding="0" cellspacing="0" border="0" style="border-radius: 3px 3px 25px 3px; background: #F3F3F3; padding: 20px; width: 400px;">
+        <table cellpadding="0" cellspacing="0" border="0" style="border-radius: 3px 3px 15px 3px; background: #F3F3F3; padding: 32px 10px; width: 300px;">
           <tr>
             <!-- QR Code -->
-            <td style="width: 50%; text-align: center;">
-              <img src="{{ $urlQr }}" alt="Código QR" width="150" height="150" style="display: block; margin: 0 auto; border: 1px solid #dddddd; border-radius: 8px;" />
+            <td style="width: 40%; text-align: center;">
+              <img src="{{ $urlQr }}" alt="Código QR" width="100" height="100" style="display: block; margin: 0 auto; border: 1px solid #dddddd; border-radius: 8px;" />
             </td>
             <!-- Text -->
-            <td style="width: 50%; text-align: left; vertical-align: middle; padding-left: 15px;">
-              <span style="color: #333; font-family: Arial; font-size: 16px; font-style: normal; font-weight: 400; line-height: normal;">
+            <td style="width: 60%; text-align: left; vertical-align: middle; padding-left: 10px;">
+              <span style="color: #333; font-family: Arial; font-size: 14px; font-style: normal; font-weight: 400; line-height: normal;">
                 Escanea el QR y accede a la WebApp
               </span>
             </td>
@@ -17,4 +17,4 @@
         </table>
       </td>
     </tr>
-  </table>
+</table>
