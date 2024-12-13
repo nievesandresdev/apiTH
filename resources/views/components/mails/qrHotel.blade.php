543878864a7f69ee60b4878b87392205bb078c10
Fran <francisco20990@gmail.com>
2024-12-13 08:36:35 -0400

    test email data qr

---
+++ resources/views/components/mails/qrHotel.blade.php
@@ -0,0 +1,20 @@
+<table width="100%" cellpadding="0" cellspacing="0" border="0" style="background-color: #f9f9f9; padding: 20px; font-family: Arial, sans-serif;">
+    <tr>
+      <td align="center">
+        <table cellpadding="0" cellspacing="0" border="0" style="border-radius: 3px 3px 25px 3px; background: #F3F3F3; padding: 20px; width: 400px;">
+          <tr>
+            <!-- QR Code -->
+            <td style="width: 50%; text-align: center;">
+              <img src="{{ $urlQr }}" alt="Código QR" width="150" height="150" style="display: block; margin: 0 auto; border: 1px solid #dddddd; border-radius: 8px;" />
+            </td>
+            <!-- Text -->
+            <td style="width: 50%; text-align: left; vertical-align: middle; padding-left: 15px;">
+              <span style="color: #333; font-family: Arial; font-size: 16px; font-style: normal; font-weight: 400; line-height: normal;">
+                Escanea el QR y accede a la WebApp
+              </span>
+            </td>
+          </tr>
+        </table>
+      </td>
+    </tr>
+  </table>
