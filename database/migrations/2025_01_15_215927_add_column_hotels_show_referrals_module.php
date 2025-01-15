bc6e4af16dd93bb8630c70b1725503644f4c958a
Fran <francisco20990@gmail.com>
2025-01-15 17:04:14 -0400

    migration referrals and beneefits

---
+++ database/migrations/2025_01_15_215927_add_column_hotels_show_referrals_module.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('hotels', function (Blueprint $table) {
+            $table->tinyInteger('show_referrals')->default(0);
+            $table->tinyInteger('offer_benefits')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('hotels', function (Blueprint $table) {
+            //
+        });
+    }
+};
