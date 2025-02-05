6372c44a263ac0d29b8a13f59d3efb4c55aa7080
Fran <francisco20990@gmail.com>
2025-02-05 18:21:16 -0400

    fix email referrals

--- resources/views/components/mails/rewards/redemSection.blade.php
+++ resources/views/components/mails/rewards/redemSection.blade.php
@@ -1,18 +1,31 @@
 <table role="presentation" width="100%" cellspacing="0" cellpadding="0"
        style="background: url('{{ asset('mails/rewards/fondo1.png') }}') no-repeat center center; background-size: cover; height: 354px;">
     <tr>
-        <td style="padding: 16px; height: 354px; vertical-align: middle; text-align: center;">
-            <div style="display: flex; align-items: center; justify-content: center; gap: 16px;">
-                <!-- Imagen del regalo -->
-                <img src="{{ asset('mails/rewards/regalo.png') }}" alt="Regalo" style="max-height: 150px; display: block;">
+        <td style="height: 354px; vertical-align: middle; text-align: center; padding: 16px;">
+            <!-- Tabla interna con dos columnas -->
+            <table role="presentation" width="100%" cellspacing="0" cellpadding="0" style="margin: 0 auto; display: inline-block; text-align: center;">
+                <tr>
+                    <!-- Columna: Imagen del regalo -->
+                    <td style="width: 50%; text-align: right; padding-right: 8px; vertical-align: middle;">
+                        <img src="{{ asset('mails/rewards/regalo.png') }}" alt="Regalo"
+                             style="max-height: 150px; display: block; margin: 0 auto;">
+                    </td>
 
-                <!-- Texto de descuento -->
-                <div style="text-align: left;">
-                    <h2 style="margin:0;color: #fff; font-family: Arial, sans-serif; font-size: 32px; font-weight: 700; line-height: 145.455%;">
-                        35%<br>de descuento
-                    </h2>
-                </div>
-            </div>
+                    <!-- Columna: Texto del descuento -->
+                    <td style="width: 50%; text-align: left; padding-left: 8px; vertical-align: middle;">
+                        <h2 style="margin: 0; font-family: Arial, sans-serif; color: #fff; line-height: 145.455%;">
+                            <span class="discount-percentage" style="font-size: 64px; font-weight: 800; display: block;">35%</span>
+                            <span class="discount-text" style="font-size: 32px; font-weight: 400; display: block; margin-top: 16px;">de descuento</span>
+                        </h2>
+                    </td>
+                </tr>
+                <tr>
+                    <!-- Espacio para el código, ignorado por ahora -->
+                    <td colspan="2" style="text-align: center; padding-top: 16px;">
+                        <div style="height: 50px;"></div>
+                    </td>
+                </tr>
+            </table>
         </td>
     </tr>
 </table>
--- resources/views/components/mails/rewards/redemSectionStyles.blade.php
+++ resources/views/components/mails/rewards/redemSectionStyles.blade.php
@@ -1,22 +1,25 @@
-<!-- Estilos Responsivos -->
 <style>
-  /* Desktop */
-  @media only screen and (min-width: 601px) {
-        .desktop-section {
-            display: block;
+    /* Desktop */
+    @media only screen and (min-width: 601px) {
+        .discount-percentage {
+            font-size: 64px;
+            font-weight: 800;
         }
-        .mobile-section {
-            display: none;
+        .discount-text {
+            font-size: 32px;
+            font-weight: 400;
         }
     }
 
     /* Mobile */
     @media only screen and (max-width: 600px) {
-        .desktop-section {
-            display: none;
+        .discount-percentage {
+            font-size: 32px;
+            font-weight: 700;
         }
-        .mobile-section {
-            display: block;
+        .discount-text {
+            font-size: 18px;
+            font-weight: 400;
         }
     }
 </style>
