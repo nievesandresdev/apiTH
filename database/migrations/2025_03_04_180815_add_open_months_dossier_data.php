72bf86783d41d8b763bfcc8188233065284f55d4
jhjacoz96 <jhjacoz96@gmail.com>
2025-03-19 15:30:33 -0400

    fix

--- database/migrations/2025_03_04_180815_add_open_months_dossier_data.php
+++ database/migrations/2025_03_04_180815_add_open_months_dossier_data.php
@@ -12,7 +12,9 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('dossier_data', function (Blueprint $table) {
-            $table->integer('openMonths')->after('occupancyRateIncrease')->default(0);
+            if (!Schema::hasColumn('dossier_data', 'openMonths')) {
+                $table->integer('openMonths')->after('occupancyRateIncrease')->default(0);
+            }
         });
     }
 
