f459e7fc3540241f10a4c5a5ce288dea693b30a7
jhjacoz96 <jhjacoz96@gmail.com>
2025-03-19 15:13:25 -0400

    fix

---
+++ database/migrations/2025_03_19_201206_change_description_in_facility_hoster_languages_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('facility_hoster_languages', function (Blueprint $table) {
+            if (Schema::hasColumn('facility_hoster_languages', 'description')) {
+                $table->text('description')->nullable()->change();
+            }
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('facility_hoster_languages', function (Blueprint $table) {
+            //
+        });
+    }
+};
