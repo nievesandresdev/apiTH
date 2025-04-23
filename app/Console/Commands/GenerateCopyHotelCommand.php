e96784471f0aa4738e45ca1d30ed30da9ab6651d
Andres <andresdreamerf@gmail.com>
2025-04-23 12:15:13 -0400

    trial dates

--- app/Console/Commands/GenerateCopyHotelCommand.php
+++ app/Console/Commands/GenerateCopyHotelCommand.php
@@ -67,20 +67,20 @@ class GenerateCopyHotelCommand extends Command
         $codeDiff = '974'; //email
         $stringDiff = 'B';
         $originalHotel = $this->cloneHotelServices->findOriginalHotel();
-        Log::info('originalHotel '.json_encode($originalHotel, JSON_PRETTY_PRINT));
+        // Log::info('originalHotel '.json_encode($originalHotel, JSON_PRETTY_PRINT));
         if(!$originalHotel) return 'No existe el Hotel';
 
         $copyChain = $this->cloneHotelServices->CreateChainToCopyHotel($originalHotel, $stringDiff);
-        Log::info('copyChain '.json_encode($copyChain, JSON_PRETTY_PRINT));
+        // Log::info('copyChain '.json_encode($copyChain, JSON_PRETTY_PRINT));
 
         $copyHotel = $this->cloneHotelServices->CreateCopyHotel($originalHotel, $stringDiff, $copyChain);
-        Log::info('copyHotel '.json_encode($copyHotel, JSON_PRETTY_PRINT));
+        // Log::info('copyHotel '.json_encode($copyHotel, JSON_PRETTY_PRINT));
 
         $copyUser = $this->cloneHotelServices->CreateCopyOwnerUser($originalHotel, $codeDiff, $copyChain, $copyHotel);
-        Log::info('copyUser '.json_encode($copyUser, JSON_PRETTY_PRINT));
+        // Log::info('copyUser '.json_encode($copyUser, JSON_PRETTY_PRINT));
 
         $updateTrialStays = $this->cloneHotelServices->UpdateTrialStays($originalHotel, $copyHotel, $copyChain);
-        Log::info('updateTrialStays '.json_encode($updateTrialStays, JSON_PRETTY_PRINT));
+        // Log::info('updateTrialStays '.json_encode($updateTrialStays, JSON_PRETTY_PRINT));
 
         $this->cloneHotelServices->SyncTranslateCopyHotel($originalHotel, $copyHotel);
         $this->cloneHotelServices->CleanRealStaysInCopyHotel($copyHotel);
@@ -92,37 +92,37 @@ class GenerateCopyHotelCommand extends Command
         $this->cloneHotelServices->SyncWifiNetworks($originalHotel, $copyHotel);
 
         $copyCustomization = $this->cloneHotelServices->CopyCustomization($originalHotel->id, $copyHotel->id, $copyChain->id);
-        Log::info('copyCustomization '.json_encode($copyCustomization));
+        // Log::info('copyCustomization '.json_encode($copyCustomization));
 
         $this->cloneFacilityService->handle($originalHotel->id, $copyHotel->id);
-        Log::info('cloneFacilityService Facility del hotel clonado');
+        // Log::info('cloneFacilityService Facility del hotel clonado');
 
         // politicas y normas generales del hotel
         $this->cloneLegalHotel->handle($originalHotel->id, $copyHotel->id);
-        Log::info('cloneLegalGeneral Legal general y normas del hotel clonado');
+        // Log::info('cloneLegalGeneral Legal general y normas del hotel clonado');
 
         // triggers de comunicaciones del hotel
         $this->cloneTriggersCommunicationsHotel->cloneHotelCommunications($originalHotel->id, $copyHotel->id);
-        Log::info('cloneTriggersCommunicationsHotel Triggers de comunicaciones del hotel clonado');
+        // Log::info('cloneTriggersCommunicationsHotel Triggers de comunicaciones del hotel clonado');
 
         // configuraciones generales del hotel (subdomain y idioma por defecto)
         $this->cloneConfigGeneral->cloneConfigGeneral($originalHotel->id, $copyHotel->id,$stringDiff);
-        Log::info('cloneConfigGeneral Configuraciones generales del hotel clonado');
+        // Log::info('cloneConfigGeneral Configuraciones generales del hotel clonado');
 
         // rewards (referentes y referidos) del hotel
         $this->cloneRewardsHotel->handle($originalHotel->id, $copyHotel->id,$copyUser->id);
-        Log::info('cloneRewardsHotel Rewards del hotel clonado');
+        // Log::info('cloneRewardsHotel Rewards del hotel clonado');
 
         // posiciones de trabajo del hotel
         $this->cloneWorkPosition->handle($originalHotel->id, $copyHotel->id);
-        Log::info('cloneWorkPosition Posiciones de trabajo del hotel clonado');
+        // Log::info('cloneWorkPosition Posiciones de trabajo del hotel clonado');
 
         // perfil del usuario del hotel
         $this->cloneProfileUser->handle($originalHotel->id, $copyHotel->id, $copyUser->id, $stringDiff);
-        Log::info('cloneProfileUser Perfil del usuario del hotel clonado');
+        // Log::info('cloneProfileUser Perfil del usuario del hotel clonado');
 
         // Crear estancia infinita para el hotel clonado
         $this->createInfiniteStay->handle($copyHotel->id);
-        Log::info('Infinite stay created for cloned hotel');
+        // Log::info('Infinite stay created for cloned hotel');
     }
 }
