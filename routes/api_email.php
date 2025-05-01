ac8fee0011cf2baf8e6f2ea6a03257c76e95a774
Fran <francisco20990@gmail.com>
2025-05-01 07:52:05 -0400

    fix

--- routes/api_email.php
+++ routes/api_email.php
@@ -6,5 +6,5 @@ use App\Http\Controllers\Api\EmailController;
 
 Route::group(['prefix' => 'email'], function () {
     Route::post('/disabledEmail', [EmailController::class, 'disabledEmail']);
-    Route::post('/reactivateEmail', [EmailController::class, 'reactivateEmail']);
+    Route::post('/reactivateEmail', [EmailController::class, 'enabledEmail']);
 });
