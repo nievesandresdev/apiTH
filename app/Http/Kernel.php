0b95d4f6df4fceee28d4e70b41b2347776a3c675
xpestana <unibasileaweb@gmail.com>
2025-05-07 14:00:19 -0400

    Rehabilita el middleware CacheResponses en Kernel.php, restaurando su uso en el grupo de rutas API tras haber sido comentado previamente para pruebas. Esto mejora la gestión de respuestas cacheadas en la aplicación.

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -15,7 +15,6 @@ class Kernel extends HttpKernel
      */
     protected $middleware = [
         // \App\Http\Middleware\TrustHosts::class,
-        \App\Http\Middleware\CacheResponses::class,
         \App\Http\Middleware\TrustProxies::class,
         \Illuminate\Http\Middleware\HandleCors::class,
         \App\Http\Middleware\PreventRequestsDuringMaintenance::class,
@@ -47,6 +46,7 @@ class Kernel extends HttpKernel
             // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \App\Http\Middleware\CacheResponses::class,
         ],
     ];
 
