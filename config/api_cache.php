8713b99a955086b478388bb739ce1b34bae821c8
xpestana <unibasileaweb@gmail.com>
2025-05-20 16:39:27 -0400

    Agregar ruta 'api/chain/verifySubdomainExist*' a la configuración de caché de la API.

--- config/api_cache.php
+++ config/api_cache.php
@@ -9,6 +9,7 @@ return [
         'api/auth/*',
         'api/guest/findByIdApi/*',
         'api/hotel/findByParams*',
+        'api/chain/verifySubdomainExist*',
         //'api/user/profile',
     ],
     
