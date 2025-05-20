b1e58a49d4356cae562b53095e53a672c4ccc2f1
xpestana <unibasileaweb@gmail.com>
2025-05-20 18:10:10 -0400

    Actualizar la configuración de exclusión de rutas en la caché de la API, añadiendo 'api/hotel/findByParams/*' y ajustando 'api/hotel/findByParams' para una mejor coincidencia.

--- config/api_cache.php
+++ config/api_cache.php
@@ -8,7 +8,8 @@ return [
     'excluded_routes' => [
         'api/auth/*',
         'api/guest/findByIdApi/*',
-        'api/hotel/findByParams*',
+        'api/hotel/findByParams',
+        'api/hotel/findByParams/*',
         'api/chain/verifySubdomainExist*',
         //'api/user/profile',
     ],
