f292c5dd4fe341e793480cce4a847947f3bda4af
xpestana <unibasileaweb@gmail.com>
2025-05-21 14:55:21 -0400

    Restaurar rutas 'api/hotel/findByParams' y 'api/hotel/findByParams/*' en la configuración de exclusión de la caché, mejorando la gestión de la caché para estas rutas específicas.

--- config/api_cache.php
+++ config/api_cache.php
@@ -4,11 +4,12 @@ return [
     'enabled' => env('CACHE_ENABLED', true),
     
     'default_ttl' => 300, // 5 minutos por defecto
-    //'api/hotel/findByParams',
-    //'api/hotel/findByParams/*',
+    
     'excluded_routes' => [
         'api/auth/*',
         'api/guest/findByIdApi/*',
+        'api/hotel/findByParams',
+        'api/hotel/findByParams/*',
         'api/chain/verifySubdomainExist*',
         //'api/user/profile',
     ],
