a415c21f1f773423deb8d99ab1e54c177a0b2246
jhjacoz96 <jhjacoz96@gmail.com>
2025-06-02 09:12:29 -0400

    fix

--- app/Jobs/UpdateTranslateReviewsJob.php
+++ app/Jobs/UpdateTranslateReviewsJob.php
@@ -11,6 +11,7 @@ use Illuminate\Queue\SerializesModels;
 use App\Models\Hotel;
 
 
+
 class UpdateTranslateReviewsJob implements ShouldQueue
 {
     
@@ -32,13 +33,18 @@ class UpdateTranslateReviewsJob implements ShouldQueue
     {
         \Log::info("Start UpdateTranslateReviewsJob");
         $codeHotels = ['f9d8c71a516af822a8b44fbe4c116f50', 'c4687e2a827e461c4037d6681c4732c5', 'c010f0b234e8868e4e08e30bfca76fb1', '67f06c5e1e9ed065e6ec5dc8960bdd8b', '7cb2e8a36a8953dce7cb229d55ba3395', '2a62888b35a87b6c90eed3757cfb7928', '7bd448c4bd8f88453c9dd4cd5d15093c', '67acad79061d0816dd3d6cb9af58cd1f', '17160715040133464864', '8163422725375425318' , '2d8d89951b907e073407a7dddaebd31c', 'f6852088ddd15f1e4ec7e77d2b11e151'];
+        // $codeHotels = ['8163422725375425318'];
+
         foreach ($codeHotels as $codeHotel) {
             $hotel = Hotel::where('code', $codeHotel)->first();
             if ($hotel) {
                 \Log::info("[Hotel: " . $hotel->name . " - Code: " . $hotel->code . " - id: " . $hotel->id . "]");
-                $this->apiReviewService->updateReviews($hotel);
-                \Log::info("[End UpdateReviewsJob]");
+                $OTAS = ['BOOKING', 'EXPEDIA', 'TRIPADVISOR', 'GOOGLE'];
+                foreach ($OTAS as $ota) {
+                    $this->apiReviewService->translateReviewsByOta($hotel, $ota);
+                }
             }
         }
+        \Log::info("End UpdateTranslateReviewsJob");
     }
 }
