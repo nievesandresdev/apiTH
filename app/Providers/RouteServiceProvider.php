dd54f921d00c69c68162b3ea5a625c158bac1998
xpestana <unibasileaweb@gmail.com>
2025-06-04 15:27:44 -0400

    Eliminar el controlador MetadataController, que gestionaba la obtención de metadatos para hoteles específicos, mejorando la limpieza del código.

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -32,18 +32,43 @@ class RouteServiceProvider extends ServiceProvider
     protected function loadApiRoutes(): void
     {
         Route::middleware(['api', 'setlocale', 'loadHotel', 'authStatic'])
-            ->group(function () {
-                $path = app_path('Providers/Routes');
-                foreach (glob($path . '/api_*.php') as $file) {
-                    $this->loadModuleRoutes(basename($file));
-                }
-            });
+             ->group(function () {
+                 $this->loadModuleRoutes('api_chatgpt.php');
+                 $this->loadModuleRoutes('api_hotel.php');
+                 $this->loadModuleRoutes('api_hotel_ota.php');
+                 $this->loadModuleRoutes('api_email.php');
+                 $this->loadModuleRoutes('api_stay.php');
+                 $this->loadModuleRoutes('api_guest.php');
+                 $this->loadModuleRoutes('api_stay_survey.php');
+                 $this->loadModuleRoutes('api_city.php');
+                 $this->loadModuleRoutes('api_rewards.php');
+                 $this->loadModuleRoutes('api_external_platforms.php');
+                //  $this->loadModuleRoutes('api_experience.php');
+                //  $this->loadModuleRoutes('api_place.php');
+                 $this->loadModuleRoutes('api_chat.php');
+                 $this->loadModuleRoutes('api_legal.php');
+                 $this->loadModuleRoutes('api_emails.php');
+                 $this->loadModuleRoutes('api_utils.php');
+                 $this->loadModuleRoutes('api_facility.php');
+                 $this->loadModuleRoutes('api_queries.php');
+                 $this->loadModuleRoutes('api_stay_access.php');
+                 $this->loadModuleRoutes('api_requests.php');
+                 $this->loadModuleRoutes('api_data_services.php');
+                 $this->loadModuleRoutes('api_tests.php');
+                 $this->loadModuleRoutes('api_gallery.php');
+                 $this->loadModuleRoutes('api_notifications.php');
+                 $this->loadModuleRoutes('api_chain.php');
+                 $this->loadModuleRoutes('api_dossier.php');
+                 $this->loadModuleRoutes('api_hotel_buttons.php');
+                 $this->loadModuleRoutes('api_metadata.php');
+                 // Aquí puedes añadir más archivos de módulos según sea necesario
+             });
     }
 
     protected function loadModuleRoutes(string $routeFile): void
     {
         Route::prefix('api')
-            ->group(base_path('routes/' . $routeFile));
+             ->group(base_path('routes/' . $routeFile));
     }
 
 }
