86252df2e5620d28c87d40bad5e4af36cc69d6b0
xavier pestana <xpestana4@gmail.com>
2025-06-04 10:47:11 -0400

    asd

--- routes/api_metadata.php
+++ routes/api_metadata.php
@@ -5,6 +5,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\Metadata\MetadataController;
 
 /*NO debe tener middlewares para poder que funcioone con el servidor de RRSS */
-Route::group(['prefix' => 'metadata'], function () {
+Route::prefix('metadata')->group(function () {
     Route::get('/{slug}', [MetadataController::class, 'show']);
 });
