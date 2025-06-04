7432329e2e7e916a53070662edd9f3d3cabf12db
xavier pestana <xpestana4@gmail.com>
2025-06-04 10:13:29 -0400

    asd

---
+++ routes/api_metadata.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\Api\Metadata\MetadataController;
+
+/*NO debe tener middlewares para poder que funcioone con el servidor de RRSS */
+
+Route::group(function () {
+    Route::group(['prefix' => 'metadata'], function () {
+        Route::get('/{slug}', [MetadataController::class, 'show']);
+    });
+});
