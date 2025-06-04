69872aca782c909a73863e9331328bd94c696a40
xavier pestana <xpestana4@gmail.com>
2025-06-04 09:37:45 -0400

    asas

--- app/Http/Controllers/Metadata/MetadataController.php
+++ app/Http/Controllers/Metadata/MetadataController.php
@@ -4,8 +4,38 @@ namespace App\Http\Controllers\Metadata;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Hotel;
 
 class MetadataController extends Controller
 {
-    //
+ /**
+     * Obtiene los metadatos para un hotel específico
+     *
+     * @param string $slug El identificador único del hotel
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function show($slug)
+    {
+        // Busca el hotel por su slug
+        $hotel = Hotel::where('slug', $slug)->first();
+        
+        // Si no se encuentra el hotel, devuelve un 404
+        if (!$hotel) {
+            return response()->json([
+                'error' => 'Hotel no encontrado'
+            ], 404);
+        }
+        
+        // Devuelve los datos estructurados para los meta tags
+        return response()->json([
+            'success' => true,
+            'data' => [
+                'title' => $hotel->name,
+                'description' => $hotel->description,
+                'image' => $hotel->image,
+                'url' => 'https://' . $slug . '.thehoster.app/',
+                'hotel_name' => $hotel->name,
+            ]
+        ]);
+    }
 }
