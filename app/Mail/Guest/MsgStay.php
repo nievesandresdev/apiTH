47ae1f00dd55f062567a6a534e3c61f26de2275d
xpestana <unibasileaweb@gmail.com>
2025-06-06 13:23:26 -0400

    Agrega registros de información en MsgStay para el smtpSender y maskEmail, mejorando la trazabilidad en el envío de correos.

--- app/Mail/Guest/MsgStay.php
+++ app/Mail/Guest/MsgStay.php
@@ -61,7 +61,8 @@ class MsgStay extends Mailable
                     ? $this->hotel->sender_mail_mask
                     : config('app.mail_sender');
         $fromName   = $this->hotel->name;
-
+        \Log::info('MsgStay → smtpSender: ' . $smtpSender);
+        \Log::info('MsgStay → maskEmail:  ' . $maskEmail);
         return new Envelope(
             from: new Address($maskEmail, $fromName),
             subject: $subject,
