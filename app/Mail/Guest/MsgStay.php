67c4aaaaa88e5e7094a53db281097c62caecc754
xpestana <unibasileaweb@gmail.com>
2025-06-06 12:11:51 -0400

    Refactoriza la clase MsgStay para implementar el uso de Envelope y Content en la construcción de correos, mejorando la claridad y la estructura del código. Se añaden tipos de datos a los parámetros del constructor y se establece el locale antes de generar el contenido del mensaje.

--- app/Mail/Guest/MsgStay.php
+++ app/Mail/Guest/MsgStay.php
@@ -5,86 +5,107 @@ namespace App\Mail\Guest;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Address;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\App;
-use Illuminate\Support\Facades\Log;
-class MsgStay extends Mailable
+
+class MsgStay extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
+
     public $type;
     public $hotel;
     public $guest;
-    public $link;
-    public $create;
-    public $urlQr;
     public $data;
     public $after;
     public $beforeCheckin;
     public $locale;
+
     /**
      * Create a new message instance.
      *
+     * @param  string       $type
+     * @param  \App\Models\Hotel  $hotel
+     * @param  \App\Models\Guest  $guest
+     * @param  mixed|null   $data
+     * @param  bool         $after
+     * @param  bool         $beforeCheckin
      * @return void
      */
     public function __construct(
-        $type,
+        string $type,
         $hotel,
         $guest,
         $data = null,
-        $after = false,
-        $beforeCheckin = false
-    )
-    {
-        $this->type = $type;
-        $this->hotel = $hotel;
-        $this->guest = $guest;
-        $this->data = $data;
-        $this->after = $after;
+        bool $after = false,
+        bool $beforeCheckin = false
+    ) {
+        $this->type          = $type;
+        $this->hotel         = $hotel;
+        $this->guest         = $guest;
+        $this->data          = $data;
+        $this->after         = $after;
         $this->beforeCheckin = $beforeCheckin;
-        $this->locale = $guest->lang_web ?? 'es';
-
+        $this->locale        = $guest->lang_web ?? 'es';
     }
 
     /**
-     * Build the message.
+     * Get the message envelope.
      *
-     * @return $this
+     * @return \Illuminate\Mail\Mailables\Envelope
      */
-    public function build()
+    public function envelope(): Envelope
     {
-
+        // 1) Asignar el locale antes de generar asunto/traducciones
         App::setLocale($this->locale);
-        //Log::info('Locale: ',['locale' => $this->locale,'lang_web guest' => $this->guest->lang_web]);
 
-        // Definir el asunto traducido según el tipo
+        // 2) Calcular el asunto según el tipo
         switch ($this->type) {
             case 'welcome':
             case 'inviteGuestFromSaas':
                 $subject = __('mail.welcome.subject', ['hotel' => $this->hotel->name]);
                 break;
+
             case 'postCheckin':
                 $subject = __('mail.postCheckin.subject');
                 break;
+
             default:
                 $subject = __('mail.default.subject');
                 break;
         }
 
-        // if($this->type == 'welcome'){
-        //     $subject = 'Hola '.$this->guest_name.', prueba la WebApp de '.$this->hotel->name.' ' ;
-        // }
-        // else if($this->create){
-        //     $subject = 'Explora y disfruta la ciudad junto a '. $this->hotel->name;
-        // }else{
-        // $subject = 'Te damos la bienvenida a '.$this->hotel->name.'. Descubre todo lo que podemos ofrecerte';
-        // }
+        // 3) Dirección que maneja SMTP (MAIL FROM técnico)
+        $smtpSender = config('app.mail_sender'); 
+        // 4) “Máscara” que ve el usuario final (Sender/Header)
+        $maskEmail = !empty($this->hotel->sender_mail_mask)
+                     ? $this->hotel->sender_mail_mask
+                     : config('app.mail_sender');
 
-        $senderName = $this->hotel->sender_for_sending_email;
-        $maskEmail = !empty($this->hotel->sender_mail_mask) ? $this->hotel->sender_mail_mask : config('app.mail_sender');
-        $ReceptorEmail = config('app.mail_sender');
-        return $this->from($ReceptorEmail, $this->hotel->name)
-                    ->sender($maskEmail)
-                    ->subject($subject)->view('Mails.guest.msgStay');
+        // 5) Nombre que se muestra como “From” (puede ser el nombre del hotel)
+        $fromName = $this->hotel->name;
 
+        return new Envelope(
+            from:   new Address($smtpSender, $fromName),
+            sender: new Address($maskEmail),
+            subject: $subject,
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     *
+     * @return \Illuminate\Mail\Mailables\Content
+     */
+    public function content(): Content
+    {
+        // Aseguramos que el locale ya esté establecido (por si alguien llama a content sin pasar por envelope)
+        App::setLocale($this->locale);
+
+        return new Content(
+            view: 'Mails.guest.msgStay'
+        );
     }
 }
