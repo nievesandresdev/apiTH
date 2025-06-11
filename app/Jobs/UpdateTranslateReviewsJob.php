b615c91b783b00e44c65e4c0cf4365f634ed8681
jhjacoz96 <jhjacoz96@gmail.com>
2025-06-11 10:39:12 -0400

    fix

--- app/Jobs/UpdateTranslateReviewsJob.php
+++ app/Jobs/UpdateTranslateReviewsJob.php
@@ -32,7 +32,7 @@ class UpdateTranslateReviewsJob implements ShouldQueue
     public function handle(): void
     {
         \Log::info("Start UpdateTranslateReviewsJob");
-        $codeHotels = ['f9d8c71a516af822a8b44fbe4c116f50', 'c4687e2a827e461c4037d6681c4732c5', 'c010f0b234e8868e4e08e30bfca76fb1', '67f06c5e1e9ed065e6ec5dc8960bdd8b', '7cb2e8a36a8953dce7cb229d55ba3395', '2a62888b35a87b6c90eed3757cfb7928', '7bd448c4bd8f88453c9dd4cd5d15093c', '67acad79061d0816dd3d6cb9af58cd1f', '17160715040133464864', '8163422725375425318' , '2d8d89951b907e073407a7dddaebd31c', 'f6852088ddd15f1e4ec7e77d2b11e151', '8cd9612630f6a0828fd3b43e4f7441d8', '886a9f9edb12bdd4ac1d58f0283b6855'];
+        $codeHotels = ['f9d8c71a516af822a8b44fbe4c116f50', 'c4687e2a827e461c4037d6681c4732c5', 'c010f0b234e8868e4e08e30bfca76fb1', '67f06c5e1e9ed065e6ec5dc8960bdd8b', '7cb2e8a36a8953dce7cb229d55ba3395', '2a62888b35a87b6c90eed3757cfb7928', '7bd448c4bd8f88453c9dd4cd5d15093c', '67acad79061d0816dd3d6cb9af58cd1f', '17160715040133464864', '8163422725375425318' , '2d8d89951b907e073407a7dddaebd31c', 'f6852088ddd15f1e4ec7e77d2b11e151', '8cd9612630f6a0828fd3b43e4f7441d8', '886a9f9edb12bdd4ac1d58f0283b6855', 'f7b378dbe2465d163bfa0ab7ebed6e0a', '69c9f24cf520f3689bf4a26368f4c4f2'];
         // $codeHotels = ['8163422725375425318'];
 
         foreach ($codeHotels as $codeHotel) {
