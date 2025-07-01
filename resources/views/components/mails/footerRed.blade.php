de5987be76739c9a7339f2de0da4093df2b4679e
Fran <francisco20990@gmail.com>
2025-07-01 11:00:39 -0400

    fix email footer

--- resources/views/components/mails/footerRed.blade.php
+++ resources/views/components/mails/footerRed.blade.php
@@ -15,7 +15,7 @@
                 {{ __('mail.footer.body7') }}
             </p>
             <p style="color: #FAFAFA;font-family: Arial;font-size: 10px;font-weight: 400;line-height: 140%;margin:0">
-                Si no está suscrito y desea volver a recibir nuestros correos, haga clic <a href="{{$data['urlFooterEmail']}}" style="font-weight: 600;text-decoration:underline;color: #FAFAFA;">aquí</a>
+                {{ __('mail.footer.body8') }} <a href="{{$data['urlFooterEmail']}}" style="font-weight: 600;text-decoration:underline;color: #FAFAFA;">{{ __('mail.footer.body9') }}</a>
             </p>
         </div>
     </div>
