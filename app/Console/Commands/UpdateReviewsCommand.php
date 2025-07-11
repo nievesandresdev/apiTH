3383a4995aebdc3bb7d7e0db273de44341b9287d
jhjacoz96 <jhjacoz96@gmail.com>
2025-07-11 10:39:57 -0400

    fix

--- app/Console/Commands/UpdateReviewsCommand.php
+++ app/Console/Commands/UpdateReviewsCommand.php
@@ -32,7 +32,7 @@ class UpdateReviewsCommand extends Command
         //     new UpdateReviewsJob($this->apiReviewService),
         //     new UpdateTranslateReviewsJob($this->apiReviewService)
         // ])->dispatch();
-        UpdateReviewsJob::dispatch($this->apiReviewService, $this->hotelService, $this->notificationDiscordService);
+        UpdateReviewsJob::dispatchSync($this->apiReviewService, $this->hotelService, $this->notificationDiscordService);
         // UpdateTranslateReviewsJob::dispatch($this->apiReviewService);
     }
 }
